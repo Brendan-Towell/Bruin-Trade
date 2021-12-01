cc4b406475e37e59d5ac5e95713d73303311b0a2
Brendan-Towell <towellb14@gmail.com>
2021-11-30 23:15:53 -0800

    token checking
    just adding token checking to the additional webpages introduced in the most recent update

--- client/src/containers/depositPage/index.js
+++ client/src/containers/depositPage/index.js
@@ -4,6 +4,9 @@ import { Deposit } from "./deposit";
 import { Userbar } from "../../components/userbar";
 
 export function DepositPage(props) {
+    if(localStorage.getItem("token") == -1){
+        window.location.href = '/loginrequired';
+    }
     return (
     <PageContainer>
         <Deposit>
--- client/src/containers/tradePage/index.js
+++ client/src/containers/tradePage/index.js
@@ -4,6 +4,9 @@ import { Userbar } from "../../components/userbar";
 import { Trade } from "./trade";
 
 export function TradePage(props) {
+    if(localStorage.getItem("token") == -1){
+        window.location.href = '/loginrequired';
+    }
     return (
     <PageContainer>
         <Trade>
