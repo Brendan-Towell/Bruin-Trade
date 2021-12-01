d5a2d0b0341b8c61773508c39ced57f8617f1e1b
Varun <varun.rathi36@gmail.com>
2021-11-30 23:08:46 -0800

    Async rendering of stockData

--- client/src/components/stockData/index.js
+++ client/src/components/stockData/index.js
@@ -46,109 +46,39 @@ class StockData extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            stockSymbol: props.stockSymbol,
-            openingPrice: 0,
-            currentPrice: 0
+            stock: props.stockSymbol,
+            quote: {}
         }
     }
 
-    componentDidMount() {
-        this.fetchOpeningPrice()
-        this.fetchCurrentPrice()
-    }
-
-    fetchOpeningPrice() {
-        const pointerToThis = this;
-        const API_KEY = 'HFNY7BHRWB1UJ9IF';
-        let API_CALL_DAILY = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${this.state.stockSymbol}&interval=5min&outputsize=compact&apikey=${API_KEY}`;
-        let stockOpeningPriceFunction = [];
-
-        fetch(API_CALL_DAILY)
-            .then(
-                function(response) {
-                    return response.json();
-                }
-            )
-            .then(
-                function(data) {
-                    for (var key in data['Time Series (Daily)']) {
-                        stockOpeningPriceFunction.push(data['Time Series (Daily)']
-                        [key]['1. open']);
-                    }
-                    pointerToThis.setState({
-                        openingPrice: stockOpeningPriceFunction[0]
-                    })
-                }
-            )
-    }
-
-    fetchCurrentPrice() {
-        const pointerToThis = this;
-        const API_KEY = 'HFNY7BHRWB1UJ9IF';
-        let API_CALL_INTRADAY = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${this.state.stockSymbol}&interval=5min&outputsize=compact&apikey=${API_KEY}`;
-        let stockCurrentPriceFunction = [];
-
-        fetch(API_CALL_INTRADAY)
-        .then(
-            function(response) {
-                return response.json();
-            }
-        )
-        .then(
-            function(data) {
-                for (var key in data['Time Series (5min)']) {
-                    stockCurrentPriceFunction.push(data['Time Series (5min)']
-                    [key]['1. open']);
-                }
-                pointerToThis.setState({
-                    currentPrice: stockCurrentPriceFunction[0]
-                })
-            }
-        )
+    async componentDidMount() {
+        const base = `https://sandbox.iexapis.com/stable/stock/${this.state.stock}`;
+        const endpath = '/quote';
+        const token = '?token=Tpk_1fa3ca794f3940949c492fee0615dcf5'
+        const url = base + endpath + token;
+        await fetch(url).then((response) => response.json()).then((data) => this.setState({quote: data}));
     }
     
     render() {
-        if (this.state.currentPrice > this.state.openingPrice) {
-            return (
-                <PercentChange>
-                    <Marginer direction="vertical" margin={20} />
-                    <PercentChangeComponent>
-                        <SubText>Daily percent change</SubText>
-                        <PositiveChangeText>+{FormatOutput((this.state.currentPrice-this.state.openingPrice)/this.state.openingPrice * 100)}%</PositiveChangeText>
-                    </PercentChangeComponent>
-                    <Marginer direction="vertical" margin={5} />
-                    <PercentChangeComponent>
-                        <SubText>Current price</SubText>
-                        <PositiveChangeText>${FormatOutput(this.state.currentPrice)}</PositiveChangeText>
-                    </PercentChangeComponent>
-                    <Marginer direction="vertical" margin={5} />
-                    <PercentChangeComponent>
-                        <SubText>Price difference</SubText>
-                        <PositiveChangeText>+${FormatOutput(this.state.currentPrice-this.state.openingPrice)}</PositiveChangeText>
-                    </PercentChangeComponent>
-                </PercentChange>
-            );
-        } else {
         return (
             <PercentChange>
                 <Marginer direction="vertical" margin={20} />
                 <PercentChangeComponent>
                     <SubText>Daily percent change</SubText>
-                    <NegativeChangeText>-{FormatOutput((this.state.currentPrice-this.state.openingPrice)/this.state.openingPrice * 100)}%</NegativeChangeText>
+                    <NegativeChangeText>-{FormatOutput(this.state.quote.changePercent)}%</NegativeChangeText>
                 </PercentChangeComponent>
                 <Marginer direction="vertical" margin={5} />
                 <PercentChangeComponent>
                     <SubText>Current price</SubText>
-                    <NegativeChangeText>${FormatOutput(this.state.currentPrice)}</NegativeChangeText>
+                    <NegativeChangeText>${FormatOutput(this.state.quote.delayedPrice)}</NegativeChangeText>
                 </PercentChangeComponent>
                 <Marginer direction="vertical" margin={5} />
                 <PercentChangeComponent>
                     <SubText>Price difference</SubText>
-                    <NegativeChangeText>-${FormatOutput(this.state.currentPrice-this.state.openingPrice)}</NegativeChangeText>
+                    <NegativeChangeText>-${FormatOutput(this.state.quote.change)}</NegativeChangeText>
                 </PercentChangeComponent>
             </PercentChange>
-            );
-        }
+        );
     }
 }
 
